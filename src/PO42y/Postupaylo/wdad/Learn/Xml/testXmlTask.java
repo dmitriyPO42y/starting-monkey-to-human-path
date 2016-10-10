2df521f6ae858c4654a53d50d2894bd852311f5a
dmitriyPO42y <tapkin20@yandex.ru>
2016-10-10 12:33:29 +0400

    remove .idea

---
+++ src/PO42y/Postupaylo/wdad/Learn/Xml/testXmlTask.java
@@ -0,0 +1,22 @@
+package PO42y.Postupaylo.wdad.Learn.Xml;
+
+import java.util.Calendar;
+
+/**
+ * Created by 1 on 09.10.2016.
+ */
+public class testXmlTask {
+
+    public static void main(String[] args) {
+        xmlTask.openDocument("F:\\right.xml");
+
+        Calendar date = Calendar.getInstance();
+
+        date.set(2011, 01, 03);
+        double total = xmlTask.earningsTotal("ivanov", date);
+        xmlTask.changeOfficiantName("Andrey","sidorov","", "ivanov");
+        xmlTask.removeDay(date);
+
+        System.out.println(total);
+    }
+}
