099267cf4c31dd175a01f579f4019c7b3dcc09cd
dmitriyPO42y <tapkin20@yandex.ru>
2016-11-11 06:53:39 +0400

    правки

--- src/PO42y/Postupaylo/wdad/data/managers/PreferencesManager.java
+++ src/PO42y/Postupaylo/wdad/data/managers/PreferencesManager.java
@@ -2,7 +2,7 @@ package PO42y.Postupaylo.wdad.data.managers;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
-
+import java.lang.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
@@ -17,33 +17,32 @@ import java.io.IOException;
  */
 public class PreferencesManager {
     private static PreferencesManager instance;
-    private String path = "C:/Users/User/IdeaProjects/starting-monkey-to-human-path/src/PO43/Ivanova/wdad/resources/configuration/appconfig.xml";
+    private String path = "C:\\Users\\1\\IdeaProjects\\starting-monkey-to-human-path\\src\\PO42y\\Postupaylo\\wdad\\resourses\\configuration\\appconfig.xml";
     Document document;
 
     PreferencesManager() throws ParserConfigurationException, IOException, SAXException {
         File file = new File(path);
         document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
     }
-
+    
     public static PreferencesManager getInstance() throws IOException, SAXException, ParserConfigurationException {
         if (instance == null)
             instance = new PreferencesManager();
         return instance;
     }
 
-    public boolean isCreateRegistry() {
-        String tegCreateRegistry = "createregistry";
-        if (document.getElementsByTagName(tegCreateRegistry).item(0).getTextContent().equals("yes"))
+    public boolean isUseCodeBaseOnly() {
+        String tegUseCodebaseOnly = "usecodebaseonly";
+        if (document.getElementsByTagName(tegUseCodebaseOnly).item(0).getTextContent().equals("yes"))
             return true;
         return false;
     }
-    private void writeDoc() throws TransformerException {
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource domSource = new DOMSource(document);
-        StreamResult streamResult = new StreamResult(new File(path));
-        transformer.transform(domSource, streamResult);
+
+    public String getRegistryAddress() {
+        String tegRegistryAddress = "registryaddress";
+        return document.getElementsByTagName(tegRegistryAddress).item(0).getTextContent();
     }
+
     public void setCreateRegistry(boolean createRegistry) throws TransformerException {
         String textContent;
         String tegCreateRegistry = "createregistry";
@@ -54,9 +53,9 @@ public class PreferencesManager {
         writeDoc();
     }
 
-    public String getRegistryAddress() {
-        String tegRegistryAddress = "registryaddress";
-        return document.getElementsByTagName(tegRegistryAddress).item(0).getTextContent();
+    public String getRegistryPort() {
+        String tegRegistryPort = "registryport";
+        return document.getElementsByTagName(tegRegistryPort).item(0).getTextContent();
     }
 
     public void setRegistryAddress(String newTextContent) throws TransformerException {
@@ -65,9 +64,9 @@ public class PreferencesManager {
         writeDoc();
     }
 
-    public String getRegistryPort() {
-        String tegRegistryPort = "registryport";
-        return document.getElementsByTagName(tegRegistryPort).item(0).getTextContent();
+    public String getPolicyPath() {
+        String tegPolicyPath = "policypath";
+        return document.getElementsByTagName(tegPolicyPath).item(0).getTextContent();
     }
 
     public void setRegistryPort(String newRegistryPort) throws TransformerException {
@@ -76,22 +75,11 @@ public class PreferencesManager {
         writeDoc();
     }
 
-    public String getPolicyPath() {
-        String tegPolicyPath = "policypath";
-        return document.getElementsByTagName(tegPolicyPath).item(0).getTextContent();
-    }
-
     public void setPolicyPath(String newPolicyPath) {
         String tegPolicyPath = "policypath";
         document.getElementsByTagName(tegPolicyPath).item(0).setTextContent(newPolicyPath);
     }
 
-    public boolean isUseCodeBaseOnly() {
-        String tegUseCodebaseOnly = "usecodebaseonly";
-        if (document.getElementsByTagName(tegUseCodebaseOnly).item(0).getTextContent().equals("yes"))
-            return true;
-        return false;
-    }
     public void setUseCodeBaseOnly(boolean newUseCodeBaseOnly) throws TransformerException {
         String tegUseCodebaseOnly = "usecodebaseonly";
         String textContent;
@@ -101,13 +89,30 @@ public class PreferencesManager {
         document.getElementsByTagName(tegUseCodebaseOnly).item(0).setTextContent(textContent);
         writeDoc();
     }
+
     public String getClassProvider(){
         String tegClassProvider="classprovider";
         return document.getElementsByTagName(tegClassProvider).item(0).getTextContent();
     }
+
     public void setClassProvider(String newClassProvider) throws TransformerException {
         String tegClassProvider="classprovider";
         document.getElementsByTagName(tegClassProvider).item(0).setTextContent(newClassProvider);
         writeDoc();
+    }
+
+    public boolean isCreateRegistry() {
+        String tegCreateRegistry = "createregistry";
+        if (document.getElementsByTagName(tegCreateRegistry).item(0).getTextContent().equals("yes"))
+            return true;
+        return false;
+    }
+
+    private void writeDoc() throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(path));
+        transformer.transform(domSource, streamResult);
     }
 }
